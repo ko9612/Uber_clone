97d635f1745c150d351b3cc75f1298aa669fb906
yeonjp <qkrwluds7998@gmail.com>
2024-08-05 18:50:45 +0900

    [BE] 기본 설정

---
+++ src/test/java/uberclone/uberbe/UberBeApplicationTests.java
@@ -0,0 +1,13 @@
+package uberclone.uberbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
