c652740ffd91ae9550b12895e4e0f3f0a02062cb
yeonjp <qkrwluds7998@gmail.com>
2024-08-06 09:02:54 +0900

    [BE} check git

---
+++ server/src/main/java/uberclone/uberbe/controller/LoginController.java
@@ -0,0 +1,11 @@
+package uberclone.uberbe.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+@RequiredArgsConstructor
+public class LoginController {
+}
